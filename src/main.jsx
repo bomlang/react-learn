cdfa1d535fd7dfe8d7a66c13e38e80967e48728e
bomlang <bomlang4211@gmail.com>
2023-08-09 11:29:01 +0900

    리액트 환경설정 with vite

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+// React 모듈을 불러오기
+// ReactDOM 모듈을 불러오기
+
+// React 요소 작성 (React API)
+// React 요소 작성 (with JSX)
+
+// App 함수 컴포넌트 만들기
+
+// ReactDOM.createRoot() 를 사용해서 DOM 요소를 ReactDOMRoot 객체로 생성
+// ReactDOMRoot.render() 메서드로 App을 화면에 표시(렌더링)
