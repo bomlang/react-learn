14bb7d04b394ca3a761293a843f17361b3e34305
bomlang <bomlang4211@gmail.com>
2023-08-17 15:52:11 +0900

    서버에서 데이터 상태표시 및 화면에 표현

--- src/pages/LearnStateAndEffects.jsx
+++ src/pages/LearnStateAndEffects.jsx
@@ -1,45 +1,47 @@
-import Spinner from "@/components/Spinner";
-import { useEffect, useState } from "react";
+import Spinner from '@/components/Spinner';
+import { useEffect, useState } from 'react';
 
 function LearnStateAndEffects() {
-
-
+  // 1. data
   const [data, setData] = useState([]);
+  // 2. isLoading
+  const [isLoading, setIsLoading] = useState(false);
+  // 3. error
+  const [error, setError] = useState(null);
 
-  const [isLoading, setIsLoading] = useState(false)
-
-  const [error, setError] = useState(null)
-  
-  useEffect(()=>{
-
+  // side effect
+  // request data
+  useEffect(() => {
     setIsLoading(true);
-    
-    async function fetchTodos(){
 
+    // async await
+    async function fetchTodos() {
+      // fetch api
       const response = await fetch(
-        'http://127.0.0.1:8090/api/collections/todos/records?page=1&perPage=2'
-      )
+        'http://127.0.0.1:8090/api/collections/todos/records'
+      );
 
-      if(!response.ok){
-        console.log(error);
+      // error?
+      if (!response.ok) {
+        // error handling
       }
-      const data = await response.json();
-      console.log( data );
 
+      const data = await response.json();
+
+      setData(data);
+      setIsLoading(false);
     }
 
+    fetchTodos();
+  }, []);
 
-
-    fetchTodos()
-
-  },[])
-  
-
-  if(isLoading){
+  if (isLoading) {
     return (
-        <Spinner size={120}
-        className="absolute z-[10000] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" />
-    )
+      <Spinner
+        size={120}
+        className="absolute z-[10000] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+      />
+    );
   }
 
   return (
@@ -47,6 +49,13 @@ function LearnStateAndEffects() {
       <h2 className="text-indigo-600 font-suit text-2xl">
         상태 및 이펙트 학습하기
       </h2>
+      {
+        data && data.items?.map(item => (
+          <div key={item.id} className="todo">
+            <strong>{item.doit}</strong>
+          </div>
+        ))
+      }
     </div>
   );
 }
