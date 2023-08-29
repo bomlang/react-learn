9938494d0ef00741570562c2f8ccdee07c8e60ac
bomlang <bomlang4211@gmail.com>
2023-08-29 15:03:35 +0900

    zustand로 store관리 및 미들웨어 학습

--- src/store/list.js
+++ src/store/list.js
@@ -1,19 +1,25 @@
 import { create } from 'zustand';
 
-// store (define state & action)
-const listStore = () => {
-  // return store's state & actions
-  return {
-    // state
-    list: [
-      {
-        id: crypto.randomUUID(),
-        title: 'Zustand는 츄~스탄트로 발음합니다.',
-      },
-    ],
-    // actions
-  };
-};
+export const useListStore = create((set) => ({
+  list: [
+    {
+      id: crypto.randomUUID(),
+      title: 'Zustand는 츄~스탄트로 발음합니다.',
+    },
+  ],
 
-// hook (bind component) ← store (define state & action)
-export const useListStore = create(listStore);
+  addItem: (newItemTitle) =>
+    set((state) => ({
+      list: [
+        ...state.list,
+        {
+          id: crypto.randomUUID(),
+          title: newItemTitle,
+        },
+      ],
+    })),
+  deleteItem: (deleteId) =>
+    set((state) => ({
+      list: state.list.filter((item) => item.id !== deleteId),
+    })),
+}));
